43bb9d3594bc41ac71ce8fb2043449cc8c9dbbe0
Li Bai <lbai@temple.edu>
2023-07-16 21:15:57 -0400

    Add files via upload

--- network.sh
+++ network.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-WIFI_SSID=eduroam
-WIFI_PSK=password_at_temple
-WIFI_USER=lbai@temple.edu
+#WIFI_USER=lbai@temple.edu # comment it out if you are using non wpa-enterprise network
+WIFI_SSID=LSSID
+WIFI_PSK=password123
 
-if [! -v "$WIFI_USER" ]; then
+sudo nmcli c add type wifi con-name workshop ifname wlan0 ssid ${WIFI_SSID}
+if [ ! -v "$WIFI_USER" ]; then
    # null or unset the user because personal wifi does not user username
-   sudo nmcli con modify workshop wifi-sec.key-mgmt wpa-psk 
+   sudo nmcli con modify workshop wifi-sec.key-mgmt wpa-psk
    sudo nmcli con modify workshop wifi-sec.psk ${WIFI_PSK} 
 else
    # wpa-enterprise wifi need set username
