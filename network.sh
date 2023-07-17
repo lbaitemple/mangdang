fe9a9e7e3818798f7e49fb7665478ccdad6bcbcf
Li Bai <lbai@temple.edu>
2023-07-16 20:35:07 -0400

    Update network.sh

--- network.sh
+++ network.sh
@@ -6,7 +6,8 @@ WIFI_USER=lbai@temple.edu
 
 if [! -v "$WIFI_USER" ]; then
    # null or unset the user because personal wifi does not user username
-   sudo nmcli con modify workshop wifi-sec.key-mgmt wpa-psk wifi-sec.psk ${WIFI_PSK} 
+   sudo nmcli con modify workshop wifi-sec.key-mgmt wpa-psk 
+   sudo nmcli con modify workshop wifi-sec.psk ${WIFI_PSK} 
 else
    # wpa-enterprise wifi need set username
    sudo nmcli con modify workshop 802-1x.eap peap 802-1x.phase2-auth mschapv2 802-1x.identity ${WIFI_USER} 802-1x.password ${WIFI_PSK} wifi-sec.key-mgmt wpa-eap
