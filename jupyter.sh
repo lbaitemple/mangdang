23efe0dd2407cc7cc0aa26f810ee666b85a4e018
Li Bai <lbai@temple.edu>
2023-07-18 21:26:33 -0400

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -44,11 +44,11 @@ sudo systemctl enable jupyter_lab
 sudo systemctl start jupyter_lab
 
 # install mangdang bsp
-cd ~
-git clone https://github.com/mangdangroboticsclub/mini_pupper_bsp
-cd mini_pupper_bsp
-./install.sh
-cd Python_Module
-sudo python3 setup.py install
+#cd ~
+#git clone https://github.com/mangdangroboticsclub/mini_pupper_bsp
+#cd mini_pupper_bsp
+#./install.sh
+#cd Python_Module
+#sudo python3 setup.py install
 # record script end time
 date
