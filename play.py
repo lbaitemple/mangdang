1a3aee30c07487ac2a16d10bedae08dbad7d9169
Li Bai <lbai@temple.edu>
2023-11-02 22:04:58 -0400

    Update play.py

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ import sys
 from playsound import playsound
 
 if __name__ == '__main__':
-  if (len(sys.argv) >0):
+  if (len(sys.argv) >1):
     playsound(sys.argv[1])
   else:
     playsound('how.mp3')
