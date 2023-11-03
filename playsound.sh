a827855099fdaa65a03ec786262c5d3eb9019a57
Li Bai <lbai@temple.edu>
2023-11-02 22:18:15 -0400

    Update playsound.sh

--- playsound.sh
+++ playsound.sh
@@ -3,9 +3,11 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install libcairo2-dev pkg-config python3-dev -y
 pip install pycairo
-sudo apt install libgirepository1.0-dev python3-qtpy -y
+sudo apt install libgirepository1.0-dev -y
 sudo apt-get install gstreamer1.0 -y
+
 
 
 pip install pygobject playsound
 
+sudo apt-get install python3-qtpy -y
