b18f20c763944db3acad4fc4b1c5119d75d774f1
Li Bai <lbai@temple.edu>
2023-11-06 09:38:17 -0500

    Update play.py

--- play.py
+++ play.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python3
 
-import sys
+import sys, os
 from playsound import playsound
 
 if __name__ == '__main__':
+  os.system("amixer -c 0 sset 'Headphone' 100%")
   if (len(sys.argv) >1):
     playsound(sys.argv[1])
   else:
