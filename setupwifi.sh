1dd53dbc728988c731e4045f2364f623fd87c82b
Li Bai <lbai@temple.edu>
2025-04-29 23:58:10 -0400

    Update setupwifi.sh

--- setupwifi.sh
+++ setupwifi.sh
@@ -13,16 +13,20 @@ if test -f "$FILE"; then
 
     if [ -n "$WIFI_AP1" ] && [ -n "$WIFI_AP1_PWD" ] && [ -n "$WIFI_AP2" ] && [ -n "$WIFI_AP2_PWD" ]; then
     # Update line 17 (assuming line 17 contains WIFI_AP1 or a placeholder)
-    sed -i.bak "17s/\"[^\"]*\"/\"$WIFI_AP1\"/" $ACCESSFILE
+    sed -i.bak "17s/\"[^\"]*\"/\"$WIFI_AP1\"/" $ACCESSFILE> $ACCESSFILE.1
+    cp $ACCESSFILE.1 $ACCESSFILE 
     
     # Update line 18 (assuming line 18 contains the password for WIFI_AP1 after "password: ")
-    sed -i.bak '18s/password:.*/password: '"$WIFI_AP1_PWD"'/' $ACCESSFILE
+    sed -i.bak '18s/password:.*/password: '"$WIFI_AP1_PWD"'/' $ACCESSFILE> $ACCESSFILE.1
+    cp $ACCESSFILE.1 $ACCESSFILE 
 
     # Update line 19 for WIFI_AP2 (assuming line 19 contains WIFI_AP2 or a placeholder)
-    sed -i.bak "19s/\"[^\"]*\"/\"$WIFI_AP2\"/" $ACCESSFILE
+    sed -i.bak "19s/\"[^\"]*\"/\"$WIFI_AP2\"/" $ACCESSFILE> $ACCESSFILE.1
+    cp $ACCESSFILE.1 $ACCESSFILE 
     
     # Update line 20 for WIFI_AP2_PWD (assuming line 20 contains the password for WIFI_AP2 after "password: ")
-    sed -i.bak '20s/password:.*/password: '"$WIFI_AP2_PWD"'/' $ACCESSFILE
+    sed -i.bak '20s/password:.*/password: '"$WIFI_AP2_PWD"'/' $ACCESSFILE> $ACCESSFILE.1
+    cp $ACCESSFILE.1 $ACCESSFILE 
     fi
 
    #if [ -n "$WIFI_AP1" ] && [ -n "$WIFI_AP1_PWD" ]; then
