4c71bd7262e7a7fb03477286195646910da0da5f
Li Bai <lbai@temple.edu>
2025-07-10 16:30:42 +0200

    Update setupwifi.sh

--- setupwifi.sh
+++ setupwifi.sh
@@ -3,6 +3,7 @@
 FILE=/boot/firmware/wireless.txt
 ACCESSFILE=/etc/netplan/50-cloud-init.yaml
 
+
 if test -f "$FILE"; then
     cp $ACCESSFILE $ACCESSFILE.bak
     source $FILE
@@ -20,6 +21,17 @@ if test -f "$FILE"; then
         sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE
     else
         sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE
+    fi
+
+    # Handle regulatory-domain
+    if grep -q "regulatory-domain:" $ACCESSFILE; then
+        # If line exists, update it with proper alignment
+        sed -i "s/regulatory-domain:.*/regulatory-domain: \"${COUNTRY_CODE}\"/" $ACCESSFILE
+    else
+        # If line doesn't exist, add it with proper alignment (assuming wlan0 is indented 8 spaces)
+        echo "did not find a line"
+        echo ${COUNTRY_CODE}
+        sed -i "/wlan0:/a \                regulatory-domain: \"${COUNTRY_CODE}\"" $ACCESSFILE
     fi
 
     rm $FILE
