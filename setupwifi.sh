60c12aec7a09b9956158723a71aaf0c2342fc871
Li Bai <lbai@temple.edu>
2025-08-03 17:07:35 -0400

    Update setupwifi.sh

--- setupwifi.sh
+++ setupwifi.sh
@@ -10,27 +10,35 @@ if test -f "$FILE"; then
 
     if [ -n "$WIFI_AP1" ] && [ -n "$WIFI_AP1_PWD" ] && [ -n "$WIFI_AP2" ] && [ -n "$WIFI_AP2_PWD" ]; then
         # Update lines 17-20 using sed -i (in-place edits)
-        sed -i "17s/\"[^\"]*\"/\"$WIFI_AP1\"/" $ACCESSFILE
-        sed -i "18s/password:.*/password: $WIFI_AP1_PWD/" $ACCESSFILE
-        sed -i "19s/\"[^\"]*\"/\"$WIFI_AP2\"/" $ACCESSFILE
-        sed -i "20s/password:.*/password: $WIFI_AP2_PWD/" $ACCESSFILE
+        sed -i "17s/\"[^\"]*\"/\"$WIFI_AP1\"/" $ACCESSFILE > $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
+        sed -i "18s/password:.*/password: $WIFI_AP1_PWD/" $ACCESSFILE > $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
+        sed -i "19s/\"[^\"]*\"/\"$WIFI_AP2\"/" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
+        sed -i "20s/password:.*/password: $WIFI_AP2_PWD/" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
     fi
 
     # Handle USEROAM/other cases
     if [ -n "$USEROAM" ]; then
-        sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE
+        sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
     else
-        sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE
+        sed -i "s/hash:.*/hash:${HASH}/; s/identity:.*/identity: ${ID}/; s/\(tusecurewireless\|eduroam\)/${AP}/" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
     fi
 
     # Only handle regulatory-domain if COUNTRY_CODE exists in wireless.txt
     if [ -n "$COUNTRY_CODE" ]; then
         if grep -q "regulatory-domain:" $ACCESSFILE; then
             # If line exists, update it with proper alignment
-            sed -i "s/regulatory-domain:.*/regulatory-domain: \"${COUNTRY_CODE}\"/" $ACCESSFILE
+            sed -i "s/regulatory-domain:.*/regulatory-domain: \"${COUNTRY_CODE}\"/" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
         else
             # If line doesn't exist, add it with proper alignment (assuming wlan0 is indented 8 spaces)
-            sed -i "/wlan0:/a \                regulatory-domain: \"${COUNTRY_CODE}\"" $ACCESSFILE
+            sed -i "/wlan0:/a \                regulatory-domain: \"${COUNTRY_CODE}\"" $ACCESSFILE> $ACCESSFILE.1
+        cp $ACCESSFILE.1 $ACCESSFILE 
         fi
     fi
 
